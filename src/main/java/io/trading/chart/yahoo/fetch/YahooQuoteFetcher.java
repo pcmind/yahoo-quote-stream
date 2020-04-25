039e708e06a0af92b4aa1f094a9484a1a2f64202
Honoré Vasconcelos <honore.vasconcelos@gmail.com>
2020-04-26 00:24:16 +0100

    fix: decode response with missing pre/post trading session
    If request request to not have pre/post trading session, data model from response is different that we hide in the decoder to have a single model coming out of java API

--- src/main/java/io/trading/chart/yahoo/fetch/YahooQuoteFetcher.java
+++ src/main/java/io/trading/chart/yahoo/fetch/YahooQuoteFetcher.java
@@ -3,6 +3,7 @@ package io.trading.chart.yahoo.fetch;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -20,12 +21,56 @@ import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
 import com.google.common.io.CharStreams;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 
 import io.trading.chart.yahoo.fetch.model.YahooResult;
+import io.trading.chart.yahoo.fetch.model.YahooTradingPeriod;
+import io.trading.chart.yahoo.fetch.model.YahooTradingPeriods;
 import lombok.Value;
 
 public class YahooQuoteFetcher {
     private static final String FINANCE_CHART = "https://query1.finance.yahoo.com/v8/finance/chart/";
+    private final Gson decoder;
+
+    public YahooQuoteFetcher() {
+        decoder = new GsonBuilder()
+            .registerTypeAdapter(YahooTradingPeriods.class, new JsonDeserializer<YahooTradingPeriods>() {
+                @Override
+                public YahooTradingPeriods deserialize(
+                    JsonElement json, Type typeOfT, JsonDeserializationContext context
+                ) throws JsonParseException {
+                    if (json.isJsonNull()) {
+                        return null;
+                    } else if (json.isJsonArray()) {
+                        //if request does not specify to get pre/post trading period response lack intermediate object
+                        final YahooTradingPeriods yahooTradingPeriods = new YahooTradingPeriods();
+                        yahooTradingPeriods.setRegular(deserializeTradingPeriod(json, context));
+                        return yahooTradingPeriods;
+                    } else {
+                        //if request does request pre/post trading session; pre/post may still be null be intermediate object is present
+                        final YahooTradingPeriods yahooTradingPeriods = new YahooTradingPeriods();
+                        final JsonObject object = json.getAsJsonObject();
+                        yahooTradingPeriods.setPre(deserializeTradingPeriod(object.get("pre"), context));
+                        yahooTradingPeriods.setRegular(deserializeTradingPeriod(object.get("regular"), context));
+                        yahooTradingPeriods.setPost(deserializeTradingPeriod(object.get("post"), context));
+                        return yahooTradingPeriods;
+                    }
+                }
+
+                private YahooTradingPeriod[][] deserializeTradingPeriod(
+                    JsonElement json,
+                    JsonDeserializationContext context
+                ) {
+                    return context.deserialize(json, YahooTradingPeriod[][].class);
+                }
+            })
+            .create();
+    }
 
     public static void main(String[] args) throws IOException {
         final YahooResult yahooResult = new YahooQuoteFetcher().fetch("CLVS", "1m", 2, true);
@@ -34,15 +79,16 @@ public class YahooQuoteFetcher {
 
     /**
      * @param symbol
-     * @param interval 1m, 5m, 15m, 30m, 1h, 4h, 1d, 1
+     * @param interval       1m, 5m, 15m, 30m, 1h, 4h, 1d, 1
      * @param daysBack
      * @param includePrePost
      * @return
      */
-    public YahooResult fetch(String symbol, String interval, Integer daysBack, boolean includePrePost) throws IOException {
+    public YahooResult fetch(String symbol, String interval, Integer daysBack, boolean includePrePost)
+        throws IOException {
         final Date time = Calendar.getInstance().getTime();
         time.setTime(time.getTime() - TimeUnit.DAYS.toMillis(daysBack));
-        return new Gson().fromJson(
+        return decoder.fromJson(
             fetchHttps(
                 get(
                     symbol,
